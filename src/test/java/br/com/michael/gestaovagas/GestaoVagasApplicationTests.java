f61401d3813c8e03f1ba8dc9913a2b1d365ce846
Michael Strahl <michaelstrahl.sm@gmail.com>
2024-04-23 05:23:21 -0300

    init project

---
+++ src/test/java/br/com/michael/gestaovagas/GestaoVagasApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.michael.gestaovagas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaoVagasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

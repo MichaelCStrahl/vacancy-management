88179df5579c2d55fbb96bc6cd2984ac27f8821b
Michael Strahl <michaelstrahl.sm@gmail.com>
2024-05-01 14:50:14 -0300

    add company and job controllers

---
+++ src/main/java/br/com/michael/gestaovagas/modules/company/repositories/CompanyRepository.java
@@ -0,0 +1,12 @@
+package br.com.michael.gestaovagas.modules.company.repositories;
+
+import java.util.Optional;
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import br.com.michael.gestaovagas.modules.company.entities.CompanyEntity;
+
+public interface CompanyRepository extends JpaRepository<CompanyEntity, UUID> {
+  Optional<CompanyEntity> findByUsernameOrEmail(String username, String email);
+}
